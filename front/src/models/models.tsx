7fa3d7316a7d26afdddbf497abe6085d94132e34
zEEtoxIIc <73819352+zEEtoxIIc@users.noreply.github.com>
2022-04-25 16:46:36 +0200

    [Profile page] added profile page and data model

---
+++ front/src/models/models.tsx
@@ -0,0 +1,15 @@
+export class userInfo {
+        firstName: string;
+        lastName: string;
+        image: string[];
+        href: string;
+        aboutMe: string;
+        dob: string;
+        email: string;
+        phoneNumber: string;
+        school: string;
+        year: string;
+        resume: any;
+        diploma: string;
+        desiredWork: string;
+}
