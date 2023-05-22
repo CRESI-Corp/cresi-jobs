2f9720aeb706ca0e3b8d26303dc4b02891bd14fc
baptiste-cassette <baptiste.cassette@viacesi.fr>
2023-05-22 16:33:05 +0200

    update page connection
    manque les testes des imputs (décalé) + retirer navbar

--- cresi-front/src/pages/connection.tsx
+++ cresi-front/src/pages/connection.tsx
@@ -1,5 +1,8 @@
 import react from 'react';
 import {Link} from "react-router-dom"
+import '../styles/connection/connection.css'
+import microsoft from "../assets/microsoft.png"
+import ent from "../assets/moodle.png"
 
 function connectionPage() {
     return (
@@ -9,42 +12,49 @@ function connectionPage() {
                 if connection page do not display navbar 
              */}
 
-            <div class="">
+            <div class="all">
                 <form action="" method="post" class="">
-                    <h1 class="">Connexion</h1>
-                    <div class="flex flex-col">
-                        <div class="">
-                            <input type="text" placeholder='Adresse email' class=''/>
-                            <i>
-                                icon
-                            </i>
+                    <h1 class="titre">Connexion</h1>
+                    <div class="">
+                        <div class="ent">
+                            <p class="">Connexion avec l'ENT</p>
+                            <img class='entlogo' src={ent}></img>
                         </div>
-                        <div class=''>
+                        <div class="micro">
+                            <img class='micrologo' src={microsoft}></img>
+                        </div>
+                        <div class="ou">
+                            <p>ou</p>
+                        </div>
+                        <div class="mail">
+                            <input type="text" placeholder='Adresse email' class=''/>
+                            <i class="fa-solid fa-user-secret"></i>
+                        </div>
+                        <div class='mdp'>
                             <input type="text" placeholder='Mot de passe' class=""/>
-                            <i>
-                                icon
-                            </i>
+                            <i class="fa-solid fa-user-secret"></i>
                         </div>
                     </div>
-                    <div class=''>
-                        <div class="">
+                    <div class='autre'>
+                        <div class="souvenir">
                             <input type="checkbox" name="rememberMe" id="rememberMe" placeholder='rememberMe' class=''/>
                             <label htmlFor="rememberMe" class="">Se souvenir de moi</label>
                         </div>
                         <Link to="/">
-                            <div  class="">
+                            <div  class="oublie">
                                 Mot de passe oublié ?
                             </div>
                         </Link>
                         
                     </div>
 
-                    <input type="button" value="Je me connecte" class=""/>
-
+                    <div class='connection'>
+                        <input type="button" value="Je me connecte" class=""/>
+                    </div>
 
                     <Link to="/registration">
-                        <div class="">
-                            Nouveau sur la plateforme ? C'est par ici
+                        <div class="registration">
+                            Nouveau sur la plateforme ? C'est par ici !
                         </div>
                     </Link>
                 </form>
