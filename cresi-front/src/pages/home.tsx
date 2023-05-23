17b48c055dbceeb7d20d48328a881e9fc822167a
KhaledAljundi <mohamedkhaled.aljundi@viacesi.fr>
2023-05-23 16:54:20 +0200

    bouton + description

--- cresi-front/src/pages/home.tsx
+++ cresi-front/src/pages/home.tsx
@@ -1,6 +1,18 @@
+import "../styles/home/home.css";
+
 function connexion() {
     return (
-        <h1 className="title">Page d'accueil</h1>
+        <body>
+            <div> <h1 className="title">Bienvenue sur Cresi Jobs</h1></div>
+
+            <div className="Description">
+                <p>Ceci est une desctiption du site</p>
+            </div>
+            <div className="Homepage_Connexion">
+                <button type="button">Inscription</button>
+                <button type="button">Connexion</button>
+            </div>
+        </body>
     );
 }
 
