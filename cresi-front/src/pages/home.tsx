f731e75924daeaa42f74145269340ac623594baa
KhaledAljundi <mohamedkhaled.aljundi@viacesi.fr>
2023-05-30 11:08:32 +0200

    Page Home

--- cresi-front/src/pages/home.tsx
+++ cresi-front/src/pages/home.tsx
@@ -4,19 +4,15 @@ function connexion() {
     return (
         <body>
 
-            <div> <h1 className="title">Bienvenue sur Cresi Jobs</h1>
+            <div className="title"> <h1 className="title">Bienvenue sur Cresi Jobs</h1>
 
                 <div className="Description">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
                 </div>
 
                 <div className="Homepage_Connexion">
-                    <input type="button" /> Connexion
+                    <input type="button"/> Se Connecter
                 </div>
-                <div className="Homepage_Inscription">
-                    <input type="button" /> Inscription
-                </div>
-
             </div>
         </body>
     );
