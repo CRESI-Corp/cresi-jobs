cc17d4b8d3ab60627697bfa929089594844e22f3
chp567 <73819652+chp567@users.noreply.github.com>
2023-07-05 17:00:49 +0200

    update

--- cresi-front/src/App.tsx
+++ cresi-front/src/App.tsx
@@ -1,29 +1,35 @@
-import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
-import Profile from "./pages/profile"
-import Home from "./pages/home"
-import Navbar from "./components/navbar";
-import Contact from "./pages/contact";
-import Connection from './pages/connection'
-import Account from './pages/account'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  return (
-    <body>
-      <header>
-        <Navbar />
-      </header>
+  const [count, setCount] = useState(0)
 
-      <div className="Cresi">
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/profile/:id' element={<Profile />} />
-          <Route path='/contact' element={<Contact />} />
-          <Route path='/connection' element={<Connection />} />
-          <Route path='/account' element={<Account />} />
-        </Routes>
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </body>
-  );
+      <h1>Vite + React + CRESI</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
