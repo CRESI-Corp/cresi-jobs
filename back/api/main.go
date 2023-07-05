67e0706d4a37e3db0d5a95d6677baab4c0fb7ac0
chp567 <73819652+chp567@users.noreply.github.com>
2023-07-05 18:42:58 +0200

    fini archi globale

---
+++ back/api/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, item := range users {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			var user User
+			_ = json.NewDecoder(r.Body).Decode(&user)
+			user.ID = params["id"]
+			users = append(users, user)
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(nil)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for index, item := range users {
+		if item.ID == params["id"] {
+			users = append(users[:index], users[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users", GetUsers).Methods("GET")
+	router.HandleFunc("/users/{id}", GetUser).Methods("GET")
+	router.HandleFunc("/users", CreateUser).Methods("POST")
+	router.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
+	router.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
